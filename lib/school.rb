7d11d1d875abd1a7c7b1857c740e7721187395d6
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-19 15:52:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
     @roster
     end 
     def add_student(student, grade)
-      roster[grade] ||= {}
-      roster[grade] << student 
+      #roster[grade] ||= {}
+      #roster[grade] << student 
     end 
   end 
