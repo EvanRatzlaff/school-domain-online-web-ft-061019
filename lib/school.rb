891b81cab5b1fc42179512e05ba162bce015ea45
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-19 15:57:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,8 +9,7 @@ class School
     @roster
     end 
     def add_student(student, grade)
-      #roster[grade] ||= {}
+      roster[grade] ||= {}
       roster[grade] << student 
-      binding.pry 
     end 
   end 
