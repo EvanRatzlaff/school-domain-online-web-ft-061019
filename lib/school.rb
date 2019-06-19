7e2179ec75f6c9b18357425841a6f5efc64f62c9
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-19 15:46:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,10 @@
 class School
   attr_reader :roster
   def initialize(school_name)
-    
-    
+    @roster = {}
+    @school_name = school_name 
   end 
+  def roster
+    @roster
+    end 
   end 
