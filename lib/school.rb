049ae126cb67082b6e54a464db63f61cf27a74f8
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-19 15:49:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,5 +7,14 @@ class School
   def roster
     @roster
     end 
-    def add_student()
+    def add_student(grade, student)
+      roster[grade] ||= {}
+      roster[grade] << student 
+    end 
+    def grade(grade)
+      roster[grade]
+    end
+    def sort 
+      self.roster.each do |grade, student| student.sort!
+    end 
   end 
